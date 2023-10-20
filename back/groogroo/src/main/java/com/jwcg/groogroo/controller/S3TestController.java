2184311db166a0fa9afda241d4fd53d0c6c45bb1
iamjione <kimjw3928@gmail.com>
2023-10-20 15:02:14 +0900

    (BE/S3)Feat: S3 설정 및 파일 업로드 기능 구현

---
+++ back/groogroo/src/main/java/com/jwcg/groogroo/controller/S3TestController.java
@@ -0,0 +1,57 @@
+package com.jwcg.groogroo.controller;
+
+import com.jwcg.groogroo.model.service.S3UploadService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Tag(name = "S3", description = "S3 테스트 API")
+@RequestMapping("/test")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@AllArgsConstructor
+@Slf4j
+public class S3TestController {
+
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    private final S3UploadService s3UploadService;
+
+    @Operation(summary = "S3 업로드 테스트", description = "S3에 multipartFile 업로드하는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "S3 업로드 성공"),
+            @ApiResponse(responseCode = "500", description = "S3 업로드 실패 - 내부 서버 오류")
+    })
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Map<String, Object>> s3Upload(@RequestPart MultipartFile multipartFile) {
+        Map<String,Object> response = new HashMap<>();
+
+        try {
+            log.info("S3에 multipartfile 업로드");
+            String fileUrl = s3UploadService.upload(multipartFile, "test");
+            log.info("S3 업로드 성공");
+            response.put("httpStatus", SUCCESS);
+            response.put("message", "S3 업로드 성공");
+            response.put("fileUrl", fileUrl);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (IOException e) {
+            log.info("S3 업로드 실패");
+            response.put("httpStatus", FAIL);
+            response.put("message", "S3 업로드 실패");
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ back/groogroo/src/main/java/com/jwcg/groogroo/model/service/S3UploadService.java
@@ -0,0 +1,87 @@
+package com.jwcg.groogroo.model.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class S3UploadService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        // MultipartFile을 File로 변환
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+        return upload(uploadFile, dirName); // S3 업로드
+    }
+
+    private String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+
+        removeNewFile(uploadFile);
+
+        return uploadImageUrl;
+    }
+
+    /**
+     * S3 업로드
+     * @param uploadFile
+     * @param fileName
+     * @return
+     */
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucket, fileName, uploadFile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
+        );
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    /**
+     * 로컬에 저장된 파일 삭제
+     * @param targetFile
+     */
+    private void removeNewFile(File targetFile) {
+        if(targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        }else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    /**
+     * 로컬에 업로드 (MultipartFile을 File로 변환)
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    private Optional<File> convert(MultipartFile file) throws  IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if(convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
+
+}
