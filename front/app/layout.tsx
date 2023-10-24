520cf02c438520b2923eb9c3746360faaf89253a
Hanna Kim <gksskrn0158@gmail.com>
2023-10-24 11:13:31 +0900

    (FE/Design)Feat: 색상 추가 및 nes.css 이식

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-// import { Inter } from 'next/font/google'
+import { Press_Start_2P } from "next/font/google";
 import localFont from "next/font/local";
 
-// const inter = Inter({ subsets: ['latin'] })
+const pressStart2P = Press_Start_2P({ weight: "400", subsets: ["latin"] });
 
 const bitBit = localFont({
   src: "../public/font/DNFBitBitv2.ttf",
@@ -43,6 +43,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {/* nes.css 이식위한 작업 */}
+      <head>
+        <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+      </head>
       <body
         className={`${bitBit.variable} ${nexonGothic.variable} ${nexonGothic_Light.variable} ${nexonGothic_Medium.variable} ${nexonGothic_Bold.variable} ${neoDuggeunmo_Pro.variable}`}
       >
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // 속성명-[커스텀할 값] 으로 테일윈드 클래스명을 직접 커스텀
+  mode: "jit",
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +14,28 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      // theme -> extend -> color구조인데도 tailwindcss 정의 색상 사용하려면 직접 지정해야해..?
+      colors: {
+        primary: "#2A7443",
+        "primary-container": "#7FAC8E",
+        secondary: "#FFF600",
+        "secondary-container": "#FFFB99",
+        "point-orange": "#FFA107",
+        "point-orange-container": "#FFC76A",
+        "point-brown": "#734700",
+        "point-bronw-container": "#AB9166",
+        error: "#B83818",
+        "error-container": "#D48874",
+        "point-pink": "#FFD1BF",
+        background: "#F9F9F9",
+        "background-container": "#E0E0E0",
+        "text-sub": "#868686",
+        "home-background": "#97D664",
+        outline: "#787D85",
+        "outline-container": "#DCE1E9",
+        frame: "#F2FFF0",
+        "linear-frame": "#C7FFBE",
       },
       fontFamily: {
         // 👇 Add CSS variables
