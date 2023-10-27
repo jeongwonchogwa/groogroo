e3c60ae1408131d7226f8dcb1f58794095bee9a2
Hanna Kim <gksskrn0158@gmail.com>
2023-10-27 16:32:29 +0900

    (FE/Component)Feat: 정원 생성 폼 css 구성,  IconButton 수정, imageSection 변경

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import MessageContainer from "./components/MessageContainer";
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-2xl">메인페이지</div>
+    <div className="w-screen h-screen bg-background-pixel bg-cover">
+      <div className="flex w-full flex-col">
+        <div className="mx-5 mt-5">
+          <MessageContainer />
+        </div>
+        <div className="text-2xl">메인페이지</div>
+      </div>
     </div>
   );
 }
