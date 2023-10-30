72784eb58ca8066a6065510343cbe942c3ed9a4e
Hanna Kim <gksskrn0158@gmail.com>
2023-10-30 17:53:04 +0900

    (FE/Garden)Feat: 취소 버튼 클릭 시 이전으로 돌아가기

--- front/app/garden/regist/page.tsx
+++ front/app/garden/regist/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import RegistDescriptionSection from "./components/RegistDescriptionSection";
 import RegistTemplateSection from "./components/RegistTemplateSection";
 import RegistNameSection from "./components/RegistNameSection";
 import Button from "@/app/components/Button";
+import { useRouter } from "next/navigation";
 
 const RegistPage = () => {
+  const router = useRouter();
+  const clickCreate = () => {
+    console.log("생성하기 버튼 클릭");
+  };
   return (
     <div className="w-screen h-screen bg-background-pixel bg-blend-overlay bg-slate-200 bg-opacity-30 bg-cover">
       <div className="flex w-full h-full">
@@ -22,8 +29,8 @@ const RegistPage = () => {
             </div>
             <div className=" mt-8">
               <div className="grid grid-flow-col gap-2">
-                <Button color="default" label="취소하기" />
-                <Button color="primary" label="생성하기" />
+                <Button color="default" label="취소하기" onClick={() => router.back()} />
+                <Button color="primary" label="생성하기" onClick={clickCreate} />
               </div>
             </div>
           </div>
