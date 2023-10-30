6b6627091882f795fa15ad3959c3851f1318dc19
yohwan <moonyh9600@gmail.com>
2023-10-30 13:49:13 +0900

    (FE/User)Feat: 시작페이지 구현

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import MessageContainer from "./components/MessageContainer";
+import Starting from "./starting";
+
 
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-background-pixel bg-cover">
       <div className="flex w-full flex-col">
         <div className="mx-5 mt-5">
-          <MessageContainer />
+          <Starting />
         </div>
         <div className="text-2xl">메인페이지</div>
       </div>
