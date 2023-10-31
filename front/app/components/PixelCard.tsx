6d0853b818435c60e709d08b2819cb988e83f30d
Hanna Kim <gksskrn0158@gmail.com>
2023-10-31 17:18:26 +0900

    (FE/Component)Feat: 모달 prop 추가

--- front/app/components/PixelCard.tsx
+++ front/app/components/PixelCard.tsx
@@ -1,24 +1,22 @@
-import { url } from "inspector";
-
 interface Props {
   content?: React.ReactNode;
   list?: string[];
+  isModal?: boolean;
 }
 
 const PixelCard = (props: Props) => {
   return (
     <div
-      className="w-fit h-fit flex flex-col border-[15px]  border-transparent"
+      className={`w-fit h-fit flex flex-col border-[15px] ${props.isModal ? "z-50" : ""} border-transparent`}
       style={{
         borderImage: `url("/assets/images/pixelBorder.png") 25`,
       }}
     >
-      {props.list
+      {props.content
+        ? props.content
+        : props.list
         ? props.list.map((element) => (
-            <div
-              className="flex flex-col bg-white p-3 pb-0 min-w-max gap-2"
-              key={element}
-            >
+            <div className="flex flex-col bg-white p-3 pb-0 min-w-max gap-2" key={element}>
               {element}
               <hr />
             </div>
