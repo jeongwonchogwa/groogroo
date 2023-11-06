1221b1f67554e8d2d30c8f800f37fcab5a54d109
MJ Kim <zxskmj@gmail.com>
2023-11-07 00:24:50 +0900

    (FE/GenAI)Feat: flask 서버 추가 - openai image 생성

---
+++ flask/app.py
@@ -0,0 +1,63 @@
+import openai
+import requests
+from flask import Flask, request, url_for, render_template_string
+
+from dotenv import load_dotenv
+import os
+
+
+# .env 파일에서 api_key 불러오기
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+cnt = 0
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/image', methods=['GET', 'POST'])
+def make_image():
+    global cnt
+    # 입력 폼에서 프롬프트 입력받으면, 
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        print('입력받은 데이터: ', prompt)
+        # prompt = "Image of a tree with a burning dot art feel"
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,    # default는 1이며 1개당 토큰이 나가고 그 이상은 돈이 개수만큼 나간다.
+            size="256x256",
+            # response_format="url"
+        )
+        print('결과: ', response)
+        image_url = response["data"][0]["url"]
+        image_data = requests.get(image_url).content
+        image_filename = f"gen_img_{cnt}.jpg"
+        image_path = os.path.join("static", "images", image_filename)
+        with open(image_path, "wb") as f:
+            f.write(image_data)
+        cnt += 1
+        image_html = f'<img src="{url_for("static", filename=f"images/{image_filename}")}">'
+        prompt_html = f'<p>{prompt}</p>'
+        return prompt_html + image_html + render_template_string('''
+            <form method="POST">
+                Prompt: <input type="text" name="prompt">
+                <input type="submit" value="Generate Image">
+            </form>
+        ''')
+        # return f'<img src="{url_for("static", filename="images/gen_img_.jpg")}">'
+    # GET 요청이 들어오면 입력 폼 렌더링
+    return '''
+    <form method="POST">
+        Prompt: <input type="text" name="prompt">
+        <input type="submit" value="Generate Image">
+    </form>
+    '''
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ flask/readme.md
@@ -0,0 +1,13 @@
+## Flask 실행하기
+
+### 가상환경 설치
+`python -m venv venv`
+
+### 가상환경 구성
+`source venv/Scripts/activate`
+
+### 필요한 라이브러리 설치
+`pip install -r requirements.txt`
+
+### 서버 실행
+`python app.py` or `flask run`
