e71246625921cf0164259b62be0fa0aa1f651c1d
Kuma93 <kyuh2002@gmail.com>
2023-11-09 14:43:47 +0900

    (BE/Flask)Feat: 프론트에서 오는 데이터, 보내는 데이터 변환 및 포함

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
-from openai import OpenAI
+import openai
+from openai import OpenAI as open_module
 import requests
 from flask import Flask, jsonify, request, url_for, render_template_string
 from flask_cors import CORS
@@ -6,11 +7,11 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import base64       # 이미지 인코딩하기 위해
-
+import time
 
 # .env 파일에서 api_key 불러오기
 load_dotenv()
-client = OpenAI(
+client = open_module(
     api_key = os.environ.get("OPENAI_API_KEY"),
 )
 
@@ -36,20 +37,12 @@ def make_image():
         # prompt += ' with a thick black line at its boundary, animation, Pixel art, no background, no fruits'
         # print('입력받은 데이터: ', prompt)
         data = request.get_json()
+        print(data)
         user_input = data.get('text')
-        if user_input:
-            ###############################################################################
-            # url 을 보내는 방식
-            # image_url = url_for("static", filename=f"images/cat.jpg", _external=True)
-            # return jsonify({'image_url': image_url}), 200
-            ###############################################################################
-            image_path = 'static/images/cat.jpg'
-            # 이미지 파일을 열고 base64로 인코딩
-            with open(image_path, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-            # base64 인코딩된 이미지 데이터를 JSON으로 변환하여 반환
-            return jsonify({ 'image_data': encoded_string }), 200
+        user_id = data.get('id')
 
+        if user_input:
+            
             prompt = 'please create animation-styled pixel art image of a '
             # 입력값이 '나무'로 끝나지 않으면 '나무'를 추가합니다.
             if not user_input.endswith('tree'):
@@ -71,16 +64,23 @@ def make_image():
                 )
                 image_url = response.data[0].url
                 image_data = requests.get(image_url).content
-                image_filename = f"gen_img_{cnt}.jpg"
+                image_filename = f"gen_img_{time.localtime().tm_year}_{time.localtime().tm_mon}_{time.localtime().tm_mday}_{time.localtime().tm_hour}{time.localtime().tm_min}{time.localtime().tm_sec}{user_id}.jpg"
                 image_path = os.path.join("static", "images", image_filename)
                 with open(image_path, "wb") as f:
                     f.write(image_data)
-
-                image_html = f'<img src="{url_for("static", filename=f"images/{image_filename}")}">'
-                prompt_html = f'<p>{prompt}</p>'
-            except:
-                prompt_html = f'<p>잘못된 요청입니다.</p>'
-                image_html = f'<img src="https://michaels.me.uk/wp-content/uploads/2017/11/custom-error.png"/>'
+                ###############################################################################
+                # url 을 보내는 방식
+                # image_url = url_for("static", filename=f"images/cat.jpg", _external=True)
+                # return jsonify({'image_url': image_url}), 200
+                ###############################################################################
+                # 이미지 파일을 열고 base64로 인코딩
+                # with open(image_path, "rb") as image_file:
+                #     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+                encoded_string = base64.b64encode(image_data).decode('utf-8')
+                # base64 인코딩된 이미지 데이터를 JSON으로 변환하여 반환
+                return jsonify({ 'image_data': encoded_string }), 200
+            except openai.error as e:
+                return jsonify({ 'error': e }), 400;
         
         else:
             print('유저 입력 데이터 없음!!')
@@ -123,4 +123,4 @@ def make_image():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
