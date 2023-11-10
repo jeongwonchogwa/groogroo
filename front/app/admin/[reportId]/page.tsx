0254886da9292371176f6376f73ea0b88050be71
iamjione <kimjw3928@gmail.com>
2023-11-10 13:08:28 +0900

    (FE/Admin)Feat: Admin 페이지 접근제한 & 404페이지 img >> Image로 변경

--- front/app/admin/[reportId]/page.tsx
+++ front/app/admin/[reportId]/page.tsx
@@ -2,20 +2,77 @@
 
 import { useState, useEffect } from 'react';
 import { Content } from  "@/app/types";
+import { redirect } from "next/navigation";
 
 const ReportDetailPage = () => {
+  // const [userToken, setUserToken] = useState<string>("");
   const [content, setContent] = useState<Content>();
+  const [certified, setCertified] = useState<boolean>(false);
+
+  // useEffect(() => {
+  //   // 세션 스토리지에서 userInfo 가져오기
+  //   const userInfoString = sessionStorage.getItem('userInfo');
+    
+  //   if(userInfoString) {
+  //       const userInfo = JSON.parse(userInfoString);
+  //       setUserToken(userInfo.state.userToken);
+  //   } else {
+  //     redirect("/not-found");
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   const tokenParts = userToken.split(".");
+  //       console.log(tokenParts[1]);
+  //       const decodedToken = JSON.parse(atob(tokenParts[1]));
+  //       console.log("Role", decodedToken.role);
+        
+  //       if(decodedToken.role !== "ROLE_ADMIN") {
+  //         redirect("/not-found");
+  //       }
+
+  //       setCertified(true);
+  //       const params = new URLSearchParams(document.location.search).get("content");
+  //       if(params!== null){
+  //         const jsonObject = JSON.parse(params);
+  //         console.log(jsonObject)
+  //         setContent(jsonObject);
+  //       }
+  // }, [userToken]);
 
   useEffect(() => {
-    const params = new URLSearchParams(document.location.search).get("content");
-    if(params!== null){
-      const jsonObject = JSON.parse(params);
-      console.log(jsonObject)
-      setContent(jsonObject);
+    const userInfoString = sessionStorage.getItem('userInfo');
+    
+    if(userInfoString) {
+        const userInfo = JSON.parse(userInfoString);
+        const tokenParts = userInfo.state.userToken.split(".");
+        console.log(tokenParts[1]);
+        const decodedToken = JSON.parse(atob(tokenParts[1]));
+        console.log("Role", decodedToken.role);
+        
+        if(decodedToken.role !== "ROLE_ADMIN") {
+          redirect("/not-found");
+        }
+
+        setCertified(true);
+    } else {
+      redirect("/not-found");
     }
   }, []);
 
+  useEffect(() => {
+    if(certified){
+      const params = new URLSearchParams(document.location.search).get("content");
+      if(params!== null){
+        const jsonObject = JSON.parse(params);
+        console.log(jsonObject)
+        setContent(jsonObject);
+      }
+    }
+  }, [certified]);
+
   return (
+    certified &&
     <div className="h-screen flex flex-col justify-center items-center">
       <div>
         <div className="font-nexonGothic_Bold text-lg">상세 내용</div>
--- front/app/not-found.tsx
+++ front/app/not-found.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function NotFound() {
   return (
     <div className="h-screen flex flex-col justify-center items-center mx-10">
-      <img src="assets/images/not_found.svg" alt="404 not found" />
+      <Image src="assets/images/not_found.svg" alt="404 not found" width={400} height={350} priority/>
       <div className="font-bitBit text-rose-500 mt-10">요청하신 페이지를 찾을 수 없습니다.</div>
     </div>
   );
