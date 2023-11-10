0f3c2bbb0f81a3680f9732d344dded8e4e89e224
zxskmj <zxskmj@gmail.com>
2023-11-10 13:03:08 +0900

    (FE/Singup)Style: starting.tsx 수정

--- front/app/enter/components/starting.tsx
+++ front/app/enter/components/starting.tsx
@@ -2,24 +2,13 @@
 
 import Button from "../../components/Button";
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 const Starting = () => {
 
-  const router = useRouter();
-
-  const handleKakaoLogin = async () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_GROOGROO_API_URL}/oauth2/authorization/kakao`
+  const handleSocialLogin = (service: string) => {
+    window.location.href = `${process.env.NEXT_PUBLIC_GROOGROO_API_URL}/oauth2/authorization/${service}`
   };
-
-  const handleGoogleLogin = async () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_GROOGROO_API_URL}/oauth2/authorization/google`
-  };
-
-  const handleNaverLogin = async () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_GROOGROO_API_URL}/oauth2/authorization/google`
-  };
-
 
   return (
     <div className="w-full flex justify-center items-center">    
@@ -35,9 +24,9 @@ const Starting = () => {
           함께 만들어가는 우리의 정원
         </p>
 				<div className="w-[90%] space-y-4 mt-32 mb-4">
-					<Button color="secondary" label="카카오톡 로그인" onClick={handleKakaoLogin}/>
-					<Button color="white" label="구글 로그인" onClick={handleGoogleLogin}/>
-					<Button color="primary" label="네이버 로그인" onClick={handleNaverLogin}/>
+					<Button color="secondary" label="카카오톡 로그인" onClick={() => handleSocialLogin('kakao')}/>
+					<Button color="white" label="구글 로그인" onClick={() => handleSocialLogin('google')}/>
+					<Button color="primary" label="네이버 로그인" onClick={() => handleSocialLogin('naver')}/>
 				</div>
 			</div>
 		</div>
