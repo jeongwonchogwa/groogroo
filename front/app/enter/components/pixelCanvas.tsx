1eed72905b3db12a3a3f016049a922cd8a0c4f5b
iamjione <kimjw3928@gmail.com>
2023-11-14 15:18:10 +0900

    (FE/Tree)Feat: 마우스 클릭한채로 이동시 색칠가능하도록 구현

--- front/app/enter/components/pixelCanvas.tsx
+++ front/app/enter/components/pixelCanvas.tsx
@@ -2,38 +2,60 @@ import React, { useState, MouseEvent } from 'react';
 
 interface PixelCanvasProps {
   selectedTool: string;
-  selectedColor: string 
+  selectedColor: string;
 }
 
-const PixelCanvas = ({selectedTool, selectedColor}:PixelCanvasProps) => {
+const PixelCanvas = ({ selectedTool, selectedColor }: PixelCanvasProps) => {
   const [grid, setGrid] = useState<string[][]>(Array.from({ length: 16 }, () => Array(16).fill('white')));
+  const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
 
   const handleCellClick = (row: number, col: number) => {
     const newGrid = [...grid];
-    if(selectedTool == "pen"){
-      newGrid[row][col] = selectedColor; // 색을 원하는 값으로 변경
-    }else{
-      newGrid[row][col] = "white";
+    if (selectedTool === 'pen') {
+      newGrid[row][col] = selectedColor;
+    } else {
+      newGrid[row][col] = 'white';
     }
-    
     setGrid(newGrid);
   };
 
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
+  const handleMouseDown = (row: number, col: number) => {
+    setIsMouseDown(true);
+    handleCellClick(row, col);
+  };
+  
+  const handleMouseOver = (row: number, col: number) => {
+    if (isMouseDown) {
+      handleCellClick(row, col);
+    }
+  };
+
+  // 마우스가 캔버스 밖으로 벗어나면 isMouseDown false로 변경
+  const handleMouseLeave = () => {
+    setIsMouseDown(false);
+  };
+
   return (
-    <div className="flex flex-col justify-center">
-      {grid.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex">
-          {row.map((col, colIndex) => (
-            <div
-              key={colIndex}
-              className="w-4 h-4 border border-gray-300 cursor-pointer"
-              style={{ backgroundColor: col }}
-              onClick={(e: MouseEvent) => handleCellClick(rowIndex, colIndex)}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
+    <div className="flex flex-col justify-center" onMouseLeave={handleMouseLeave} onMouseUp={handleMouseUp}>
+    {grid.map((row, rowIndex) => (
+      <div key={rowIndex} className="flex">
+        {row.map((col, colIndex) => (
+          <div
+            key={colIndex}
+            className="w-4 h-4 border border-gray-300 cursor-pointer"
+            style={{ backgroundColor: col }}
+            onClick={() => handleCellClick(rowIndex, colIndex)}
+            onMouseDown={() => handleMouseDown(rowIndex, colIndex)}
+            onMouseOver={() => handleMouseOver(rowIndex, colIndex)}
+          />
+        ))}
+      </div>
+    ))}
+  </div>
   );
 };
 
