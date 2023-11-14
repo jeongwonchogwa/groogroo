c7e8389fc8bd6d85fe2a8d9c08a2028e53242dcc
yohwan <moonyh9600@gmail.com>
2023-11-14 15:45:12 +0900

    (FE/Alarm)Feat: 알람list 조회 API연결

--- front/app/components/Alarm.tsx
+++ front/app/components/Alarm.tsx
@@ -3,45 +3,39 @@
 import { useEffect, useState } from "react";
 import Button from "./Button";
 import PixelCard from "./PixelCard";
-
-const alarmData = [
-  {
-    type: "fruit",
-    writerNickname: "문요환",
-    content:
-      "오늘은 마라샹궈 먹는날 츄릅 오늘은 마라샹궈 먹는날 츄릅오늘은 마라샹궈 먹는날 츄릅오늘은 마라샹궈 먹는날 츄릅오늘은 마라샹궈 먹는날 츄릅",
-    createTime: "11월 1일",
-  },
-  {
-    type: "fruit",
-    writerNickname: "김지원",
-    content: "슬릭백 춤 개고수. 공중부양 허경영인줄",
-    createTime: "11월 1일",
-  },
-  {
-    type: "flower",
-    gardenInfo: { gardenId: 1, name: "쉿 몽환의 숲" },
-    writerNickname: "",
-    content: "김한나 힘 너무 쎄요. 무서워요.",
-    createTime: "11월 1일",
-  },
-  {
-    type: "fruit",
-    writerNickname: "차차아버님",
-    content: "자기소개서 언제쓰지 ㅎ",
-    createTime: "11월 2일",
-  },
-  {
-    type: "garden",
-    writerNickname: "김지원",
-    gardenInfo: { gardenId: 1, name: "쉿 몽환의 숲" },
-  },
-];
+import { userInfoStore } from '../../stores/userInfoStore';
 
 const AlarmList = () => {
   // window null 방지를 위해 useEffect 처리
+  const [alarmData, setAlarmData] = useState<any[]>([]);
   const [wWidth, setWwidth] = useState(window.innerWidth);
+
+  const getUserToken = () => {
+		const { userToken } = userInfoStore.getState();
+		return userToken;
+	}
+	const AccessToken = getUserToken();
+  // const AccessToken = localStorage.getItem("access_token");
+
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_GROOGROO_API_URL}/notification/list`,{
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${AccessToken}`,
+          },
+        });
+        const data = await res.json();
+        console.log(data.gardenInfo)
+        setAlarmData(data.gardenInfo);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+
     if (!window) {
       return;
     }
@@ -53,37 +47,39 @@ const AlarmList = () => {
       <div className="align py-2 px-5 text-[24px] font-bitBit">알람</div>
       <div>
         {alarmData.map((alarm, index) => {
+          const createDate = new Date(alarm.createTime);
+          const formattedDate = `${createDate.getMonth() + 1}월${createDate.getDate()}일`;
           return (
             <div key={index}>
-              {alarm.type === "fruit" ? (
+              {alarm.type === "FRUIT" ? (
                 <div className="flex flex-col w-full p-3 gap-2">
                   <div className="flex w-full justify-between items-baseline ">
                     <div className="font-nexonGothic_Bold text-base">
                       내 나무에 열매가 달렸습니다.
                     </div>
                     <div className="font-nexonGothic_Light text-sm">
-                      {alarm.createTime}
+                      {formattedDate}
                     </div>
                   </div>
                   <div className="font-nexonGothic_Light text-base overflow-hidden whitespace-nowrap text-ellipsis">
                     {alarm.content}
                   </div>
                 </div>
-              ) : alarm.type === "flower" ? (
+              ) : alarm.type === "FLOWER" ? (
                 <div className="flex flex-col w-full p-3 gap-2">
                   <div className="flex w-full justify-between items-baseline">
                     <div className="font-nexonGothic_Bold text-base">
                       {alarm.gardenInfo?.name} 에 꽃이 심어졌습니다.
                     </div>
                     <div className="font-nexonGothic_Light text-sm">
-                      {alarm.createTime}
+                      {formattedDate}
                     </div>
                   </div>
                   <div className="font-nexonGothic_Light text-base">
                     {alarm.content}
                   </div>
                 </div>
-              ) : alarm.type === "garden" ? (
+              ) : alarm.type === "GARDEN_REQUEST" ? (
                 <div className="flex w-full justify-between items-center">
                   <div className="w-[180px] font-nexonGothic_Bold h-fit">
                     {alarm.writerNickname} 님이 {alarm.gardenInfo?.name}에
