63e5dea1d1fd9334f1bd12bc1414216665d5c234
iamjione <kimjw3928@gmail.com>
2023-11-15 11:30:30 +0900

    (BE/Garden)Feat: 정원 생성 API Response 변경
    - ResponseGardenGenerationDTo 만들어서 gardenId도 같이 반환하도록 수정

---
+++ back/groogroo/src/main/java/com/jwcg/groogroo/model/dto/garden/ResponseGardenGenerationDto.java
@@ -0,0 +1,15 @@
+package com.jwcg.groogroo.model.dto.garden;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseGardenGenerationDto {
+    private Long gardenId;
+    private String url;
+}
