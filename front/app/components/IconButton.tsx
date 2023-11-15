340960b787edafeff2a2b8ac9ff2fba292c98325
jay <jjwoong1733@gmail.com>
2023-11-16 08:50:31 +0900

    (FE/Garden)Feat: 아이콘버튼 수정

--- front/app/components/IconButton.tsx
+++ front/app/components/IconButton.tsx
@@ -17,6 +17,9 @@ import Menu from "/public/assets/images/menu.svg";
 import User from "/public/assets/images/user.png";
 import Back from "/public/assets/images/back.svg";
 import Move from "/public/assets/images/move.svg";
+import Close from "/public/assets/images/close.svg";
+import Speaker from "/public/assets/images/speaker.svg";
+import SpeakerDisabled from "/public/assets/images/speaker_disabled.svg";
 
 interface IconButtonProps {
   iconSrc:
@@ -62,6 +65,9 @@ const iconButtonConfig = {
   user: User,
   back: Back,
   move: Move,
+  close: Close,
+  speaker: Speaker,
+  speaker_disabled: SpeakerDisabled,
 };
 
 const IconButton = ({ iconSrc, onClick, rotate }: IconButtonProps) => {
