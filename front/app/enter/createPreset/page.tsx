d5ee5224c8f8b845b6d152637856a7c7788b02b8
Manna-na <gksskrn0158@gmail.com>
2023-11-16 16:20:30 +0900

    Fix: createPrsetPage 오타 수정

--- front/app/enter/createPreset/page.tsx
+++ front/app/enter/createPreset/page.tsx
@@ -1,6 +1,6 @@
 import CreatePreset from "../components/createPreset";
 
-const CreatPresetpage = () => {
+const CreatePresetPage = () => {
   return (
     <div className="w-screen h-screen bg-background-home bg-cover">
       <div className="mx-10 mb-8">
@@ -10,4 +10,4 @@ const CreatPresetpage = () => {
   );
 };
 
-export default CreatePresetpage;
+export default CreatePresetPage;
