b7c64ac2ec660d00eb6c0c78816fcb2d3ad8cae9
iamjione <kimjw3928@gmail.com>
2023-11-19 22:17:25 +0900

    (FE/Garden)Fix: 카카오톡 공유하기 수정

--- front/app/garden/[gardenId]/components/GardenHeader.tsx
+++ front/app/garden/[gardenId]/components/GardenHeader.tsx
@@ -84,31 +84,38 @@ const GardenHeader = (props: Props) => {
 
   const kakaoInvite = () => {
     const { Kakao, location } = window;
-    Kakao.Share.sendDefault({
-      objectType: "feed",
-      content: {
-        title: "정원에 초대되셨습니다!🌳",
-        description: `함께 ${props.garden.name}을(를) 가꾸어 보아요!🌼🌷`,
-        // 추후 s3에 올라올 logo 사진으로 변경해야함
-        imageUrl:
-          "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
-        link: {
-          // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-          webUrl: "groogroo.site",
-          mobileWebUrl: "groogroo.site",
-        },
-      },
-
-      buttons: [
-        {
-          title: "정원 꾸미러 바로가기",
-          link: {
-            webUrl: location.href,
-            mobileWebUrl: location.href,
-          },
-        },
-      ],
+  
+    Kakao.Share.sendCustom({
+      templateId: 100953,
+      templateArgs: { garden_name: `${props.garden.name}` },
     });
+     
+    // const { Kakao, location } = window;
+    // Kakao.Share.sendDefault({
+    //   objectType: "feed",
+    //   content: {
+    //     title: "정원에 초대되셨습니다!🌳",
+    //     description: `함께 ${props.garden.name}을(를) 가꾸어 보아요!🌼🌷`,
+    //     // 추후 s3에 올라올 logo 사진으로 변경해야함
+    //     imageUrl:
+    //       "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+    //     link: {
+    //       // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+    //       webUrl: "groogroo.site",
+    //       mobileWebUrl: "groogroo.site",
+    //     },
+    //   },
+
+    //   buttons: [
+    //     {
+    //       title: "정원 꾸미러 바로가기",
+    //       link: {
+    //         webUrl: location.href,
+    //         mobileWebUrl: location.href,
+    //       },
+    //     },
+    //   ],
+    // });
   };
 
   useEffect(() => {
