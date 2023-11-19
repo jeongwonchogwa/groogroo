c389a81a86ca305adb57681ec04f0c83892725b7
Jaewoong Jeong <jjwoong1733@gmail.com>
2023-11-19 17:18:55 +0900

    (FE/Garden)Fix: 텍스쳐 교체 조건문 삭제

--- front/app/garden/[gardenId]/components/TreeEditScene.tsx
+++ front/app/garden/[gardenId]/components/TreeEditScene.tsx
@@ -179,14 +179,20 @@ export default class TreeEditScene extends Scene {
       } else {
         console.log("나무바꾸기 실행 - 이동시", modifyTreeUrl)
         this.load.image("modifyImage", modifyTreeUrl);
-        this.load.on("filecomplete-modifyImage", () => {
           trees.forEach((tree) => {
             if (tree.id === this.modifyTreeId) {
               console.log("이동할 나무 잡았음. 텍스쳐 교체하러 왔ㅇ므.")
               tree.sprite.setTexture("modifyImage");
             }
           });
-        });
+        // this.load.on("filecomplete-modifyImage", () => {
+        //   trees.forEach((tree) => {
+        //     if (tree.id === this.modifyTreeId) {
+        //       console.log("이동할 나무 잡았음. 텍스쳐 교체하러 왔ㅇ므.")
+        //       tree.sprite.setTexture("modifyImage");
+        //     }
+        //   });
+        // });
       }
     };
 
