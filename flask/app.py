423369708a13290bccb2c115bc36fcd2f7e200cd
Kuma93 <kyuh2002@gmail.com>
2023-11-22 21:56:52 +0900

    Test: 파라미터 변경

--- flask/app.py
+++ flask/app.py
@@ -29,7 +29,7 @@ def remove_background(image_src, image_path, image_filename, type):
         output_image = remove(
                 resized_image,
                 alpha_matting=True,
-                alpha_matting_foreground_threshold=180,
+                alpha_matting_foreground_threshold=200,
                 alpha_matting_background_threshold=10,
                 alpha_matting_erode_structure_size=10,
                 alpha_matting_base_size=1300,
