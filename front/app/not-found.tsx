df2244f33965ca7aec50b9919584fd122c05582c
iamjione <kimjw3928@gmail.com>
2023-11-22 13:26:18 +0900

    (FE/Not-found)Fix: 이미지 경로 수정

--- front/app/not-found.tsx
+++ front/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const NotFound = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center mx-10">
-      <Image src="assets/images/not_found.svg" alt="404 not found" width={400} height={350} priority/>
+      <Image src="/assets/images/not_found.svg" alt="404 not found" width={400} height={350} priority/>
       <div className="font-bitBit text-rose-500 mt-10">요청하신 페이지를 찾을 수 없습니다.</div>
     </div>
   );
