5bf50fb8af78a3797f03914c296e8caf5e08683f
jay <jjwoong1733@gmail.com>
2023-11-27 12:24:08 +0900

    (ETC/Docs)Feat: 서비스 화면 추가

--- front/app/components/Input.tsx
+++ front/app/components/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 //Props로 받아올 변수들
 interface InputProps {
@@ -6,25 +6,28 @@ interface InputProps {
   purpose: "title" | "content" | "name";
 }
 
-
 const Input: React.FC<InputProps> = ({ placeholder, purpose }) => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setText(e.target.value);
   };
 
   // 각 purpose에 따른 스타일 설정
-  const inputConfig: { [key in InputProps['purpose']]: string } = {
-    title:  // Title 스타일 클래스명
+  const inputConfig: { [key in InputProps["purpose"]]: string } = {
+    // Title 스타일 클래스명
+    title:
       "w-[340px] h-[65px] text-gray-800 text-2xl font-nexonGothic-Bold p-2 placeholder-center",
-    content: //content 스타일 클래스명
+    //content 스타일 클래스명
+    content:
       "w-[340px] h-[230px] text-gray-800 text-lg font-nexonGothic-Bold p-2 placeholder-center resize-y",
-    name: // Name 스타일 클래스명
-      "w-[290px] h-[50px] text-gray-800 text-lg font-bitBit p-2 placeholder-center",
+    // Name 스타일 클래스명
+    name: "w-[290px] h-[50px] text-gray-800 text-lg font-bitBit p-2 placeholder-center",
   };
   return (
-    <div className="p-4"> 
+    <div className="p-4">
       {/* content일때는 textarea, 나머지는 input 반환 */}
       {purpose === "content" ? (
         <textarea
@@ -33,7 +36,7 @@ const Input: React.FC<InputProps> = ({ placeholder, purpose }) => {
           value={text}
           onChange={handleInputChange}
           rows={4} // 자동 줄 바꿈을 위해 설정한 행 수
-          style= {{ resize: "none" }}
+          style={{ resize: "none" }}
         />
       ) : (
         <input
